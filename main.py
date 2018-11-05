a4ba8b14086754cb7e196b33849e60ffb718eecc
jliu2179 <jliu2179@gmail.com>
2018-11-04 22:27:10 -0500

    finished process for forward and backward propagation

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import math
 
+debug = False
+
+
+
 def sigmoid_squash(s):
     sigmoid = 1.0 / (1.0 + math.exp(-s))
     return sigmoid
@@ -13,20 +17,40 @@ def sigma(inputs, weights):
     return sigma
 
 
+def print_val(layer):
+    i = 0
+    for neuron in layer:
+        print("neuron {} val: {}".format(i, neuron.val))
+        i+=1
+
+def print_weights(weights):
+    i = 0
+    for weight in weights:
+        print("weight {}: {}".format(i, weight.val))
+        i+=1
+
+def print_error_terms(layer):
+    i = 0
+    for neuron in layer:
+        print("neuron {} error_term: {}".format(i, neuron.error_term))
+        i+=1
+
+
 class InputNeuron:
     def __init__(self, val):
         self.val = val
-
 
 class Weight:
     def __init__(self, val):
         self.val = val
 
+    def change_weight(self, learning_rate, error_term, val):
+        weight_change = self.val + (learning_rate * error_term * val)
+        self.val = weight_change
 
 class HiddenNeuron:
     def __init__(self):
         self.val = 0;
-        self.backward_val = 0;
 
     def calc_forward(self, inputs, weights):
         self.val = sigmoid_squash(sigma(inputs, weights))
@@ -37,16 +61,15 @@ class HiddenNeuron:
         sigma = 0
         for output_neuron, weight in zip(output_layer, weights):
             sigma += output_neuron.error_term * weight.val
-            print("output_neuron error term: {}, weight: {}".format(output_neuron.error_term, weight.val))    
-        print("sigma: {}".format(sigma))
-        self.h = sigma * (self.val * (1 - self.val))
-        print("E: {}, 1-E: {}".format(self.val, 1 - self.val))
-        print("H: {}".format(self.h))
+            # print("output_neuron error term: {}, weight: {}".format(output_neuron.error_term, weight.val))    
+        # print("sigma: {}".format(sigma))
+        self.error_term = sigma * (self.val * (1 - self.val))
+        # print("E: {}, 1-E: {}".format(self.val, 1 - self.val))
+        # print("H: {}".format(self.error_term))
 
 class OutputNeuron:
     def __init__(self):
         self.val = 0
-        self.backward_val = 0
 
     def calc_forward(self, inputs, weights):
         self.val = sigmoid_squash(sigma(inputs, weights))
@@ -69,19 +92,17 @@ class ANN:
         self.hidden_layer[0].calc_forward(self.input_layer, self.test_set.i_h_weights1)
         self.hidden_layer[1].calc_forward(self.input_layer, self.test_set.i_h_weights2)
 
-        i = 1
-        for hidden_neuron in self.hidden_layer:
-            print("hidden {}: {}".format(i, hidden_neuron.val))
+
+        if debug:
+            print("forward propagation hidden layer:")
+            print_val(self.hidden_layer)
 
         # hidden -> output layer output:
         self.test_set.output_layer[0].calc_forward(self.hidden_layer, self.test_set.h_o_weights1)
         self.test_set.output_layer[1].calc_forward(self.hidden_layer, self.test_set.h_o_weights2)
 
-
-        i = 1 
-        for output_neuron in self.output_layer:
-            print("output {}: {}".format(i, output_neuron.val))
-            i+=1
+        print("forward propagation output layer:")
+        print_val(self.output_layer)
 
     def backward_propagation(self):
 
@@ -89,23 +110,43 @@ class ANN:
         self.output_layer[0].calc_error_term(self.test_set.target1)
         self.output_layer[1].calc_error_term(self.test_set.target2)
 
-        i = 1
-        for output_neuron in self.output_layer:
-            print("output {} error term: {}".format(i, output_neuron.error_term))
-            i += 1
+        if debug:
+            print_error_terms(self.output_layer)
 
+        # calculate the error terms for the hidden layer
         backward_o_h_weights1 = [self.test_set.h_o_weights1[0], self.test_set.h_o_weights2[0]]
         backward_o_h_weights2 = [self.test_set.h_o_weights1[1], self.test_set.h_o_weights2[1]]
 
         self.hidden_layer[0].calc_backward(self.output_layer, backward_o_h_weights1)
         self.hidden_layer[1].calc_backward(self.output_layer, backward_o_h_weights2)
 
-        i = 1
-        for hidden_neuron in self.hidden_layer:
-            print("hidden {} error term: {}".format(i, hidden_neuron.h))
-            i += 1
+        if debug:
+            print_error_terms(self.hidden_layer)
 
+
+        # calculate the hidden -> output weight changes
+        for weight in self.test_set.h_o_weights1:
+            weight.change_weight(self.test_set.learning_rate, self.output_layer[0].error_term, self.output_layer[0].val)
+
+        for weight in self.test_set.h_o_weights2:
+            weight.change_weight(self.test_set.learning_rate, self.output_layer[1].error_term, self.output_layer[1].val)
+
+        # printout the new hidden -> output weight values
+        if debug:
+            print_weights(self.test_set.h_o_weights1)
+            print_weights(self.test_set.h_o_weights2)
+
+        # calculate the input -> hidden weight changes
+        for weight in self.test_set.i_h_weights1:
+            weight.change_weight(self.test_set.learning_rate, self.hidden_layer[0].error_term, self.hidden_layer[0].val)
         
+        for weight in self.test_set.i_h_weights2:
+            weight.change_weight(self.test_set.learning_rate, self.hidden_layer[1].error_term, self.hidden_layer[1].val)
+
+        # printout the new input -> hidden weight values
+        if debug:
+            print_weights(self.test_set.i_h_weights1)
+            print_weights(self.test_set.i_h_weights2)
 
 class TestSet:
     def __init__(self):
@@ -135,6 +176,12 @@ class TestSet:
         self.target1 = 1
         self.target2 = 0
 
-ann = ANN(TestSet())
-ann.forward_propagation()
-ann.backward_propagation()
+        # learning rate
+        self.learning_rate = 1.2
+
+
+if __name__ == "__main__":
+    ann = ANN(TestSet())
+    for x in range(0, 1):
+        ann.forward_propagation()
+        ann.backward_propagation()
