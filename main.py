f12fe807daf45d1686bef16288cdd7c8311c3b4d
jliu2179 <jliu2179@gmail.com>
2018-11-14 23:27:55 -0500

    added dict version

--- main.py
+++ main.py
@@ -2,110 +2,114 @@ import math
 
 debug = True
 
-# performs sigmoid operation with s as its exponent
+learning_rate = 0.1
+targetG = 1
+targetH = 0
+
+input_neuron = {
+    'A': 10,
+    'B': 30,
+    'C': 20,
+    'D': 0,
+
+}
+
+weight = {
+    'A to E': 0.2, 
+    'B to E': -0.1,
+    'C to E': 0.4,
+    'D to E': 0,
+    
+    'A to F': 0.7, 
+    'B to F': -1.2,
+    'C to F': 1.2,
+    'D to F': 0,
+
+    'E to G': 1.1,
+    'F to G': 0.1,
+    'E to H': 3.1,
+    'F to H': 1.17
+}
+
+
+hidden_neuron = {
+    'E': 0,
+    'F': 0,
+}
+
+output_neuron = {
+    'G':0,
+    'H':0
+}
+
+
+def forward_propagation():
+    # calculate the hidden layer neurons:
+    hidden_neuron['E'] = calc_E_output()
+    hidden_neuron['F'] = calc_F_output()
+    
+    if debug:
+        print('Hidden neuron E: {}, F {}'.format(hidden_neuron['E'], hidden_neuron['F']))
+    
+    # calculate the output lyaer neurons:
+    output_neuron['G'] = calc_G_output()
+    output_neuron['H'] = calc_H_output()
+
+    if debug:
+        print('Output neuron G: {}, H: {}'.format(output_neuron['G'], output_neuron['H']))
+
+def calc_E_output():
+    sigma = (input_neuron['A'] * weight['A to E']) + (input_neuron['B'] * weight['B to E']) + (input_neuron['C'] * weight['C to E'])# + (input_neuron['D'] * weight['D to E'])
+    return sigmoid(sigma)
+
+def calc_F_output():
+    sigma = (input_neuron['A'] * weight['A to F']) + (input_neuron['B'] * weight['B to F']) + (input_neuron['C'] * weight['C to F'])# + (input_neuron['D'] * weight['D to F'])
+    return sigmoid(sigma)
+
+def calc_G_output():
+    sigma = (hidden_neuron['E'] * weight['E to G']) + (hidden_neuron['F'] * weight['F to G'])
+    return sigmoid(sigma)
+
+def calc_H_output():
+    sigma = (hidden_neuron['E'] * weight['E to H']) + (hidden_neuron['F'] * weight['F to H'])
+    return sigmoid(sigma)
+
 def sigmoid(s):
-    sigmod = 1.0/(1.0 + math.exp(-s))
-    return sigmoid
+    return 1.0/(1.0 + math.exp(-s))
 
 
-# returns an array of every line in the file
-def parse_file(to_parse_file):
-    return [line.rstrip('\n') for line in open(to_parse_file)]
+def backward_propagation():
+    # calculate the error term 
+    G_error_term = output_neuron['G'] * (1 - output_neuron['G']) * (targetG - output_neuron['G'])
+    H_error_term = output_neuron['H'] * (1 - output_neuron['H']) * (targetH - output_neuron['H'])
+
+    if debug:
+        print('Error term G: {}, H: {}'.format(G_error_term, H_error_term))
+    
+    # calculate the hidden layer error term
+    E_error_term = ((weight['E to G'] * G_error_term) + (weight['E to H'] * H_error_term)) * (hidden_neuron['E'] * (1 - hidden_neuron['E']))
+    F_error_term = ((weight['F to G'] * G_error_term) + (weight['F to H'] * H_error_term)) * (hidden_neuron['F'] * (1 - hidden_neuron['F']))
+
+    if debug:
+        print('Error term E: {}, F: {}'.format(E_error_term, F_error_term))
+
+    # change the weights between the input and hidden layers'
+    weight['A to E'] = weight['A to E'] + learning_rate * E_error_term * input_neuron['A']
+    weight['B to E'] = weight['B to E'] + learning_rate * E_error_term * input_neuron['B']
+    weight['C to E'] = weight['C to E'] + learning_rate * E_error_term * input_neuron['C']
+    weight['D to E'] = weight['D to E'] + learning_rate * E_error_term * input_neuron['D']
+
+    
+    weight['A to F'] = weight['A to F'] + learning_rate * F_error_term * input_neuron['A']
+    weight['B to F'] = weight['B to F'] + learning_rate * F_error_term * input_neuron['B']
+    weight['C to F'] = weight['C to F'] + learning_rate * F_error_term * input_neuron['C']
+    weight['D to F'] = weight['D to F'] + learning_rate * F_error_term * input_neuron['D']
+    
+
+    if debug:
+        print("New weights A to E: {}, B to E: {}, C to E: {}".format(weight['A to E'], weight['B to E'], weight['C to E']))
+        print("New weights A to F: {}, B to F: {}, C to F: {}".format(weight['A to F'], weight['B to F'], weight['C to F']))
 
 
-
-
-
-
-class InputNeuron:
-    def __init__(self, val):
-        self.val = val
-
-class Weight:
-    def __init__(self, val):
-        self.val = val
-
-class HiddenNeuron:
-    def __init__(self):
-        self.val = 0
-
-class OutputNeuron:
-    def __init__(self):
-        self.val = 0
-
-
-
-
-class ANN:
-    def __init__(self):
-        pass
-
-    def generate_test_set(self):
-        self.input_layers = self.__retrieve_inputs("inputs_data.txt")
-        self.input_to_hidden_weights_1 = self.__retrieve_initial_weights("initial_weights.txt")
-
-
-
-    # stores the inputs from "initial_weights.txt" into input_layers[]
-    # on private because this function should not be called after the
-    # first call
-    def __retrieve_inputs(self, inputs_file):
-        self.input_data = parse_file(inputs_file)
-        
-        if debug:
-            print("input_data: ", self.input_data)
-
-        input_layers = []
-        for inputs in self.input_data:
-            input_layer = []
-            for bit in list(inputs):
-                input_layer.append(InputNeuron(int(bit)))
-            input_layers.append(input_layer)
-
-        return input_layers
-
-    # retrives the weights stored in "initial_weights.txt"
-    def __retrieve_initial_weights(self, weights_file):
-        weights_data = parse_file(weights_file)
-
-        if debug:
-            print("weights: ", weights_data)
-
-        input_to_hidden1_weights = []
-        input_to_hidden2_weights = []
-        
-        total_weights = []
-        for w in range(0, 4):
-            total_weights.append(weights_data[w].split())
-
-        if debug:
-            print("weights:", total_weights)
-
-        for w in range(0, 4):
-            input_to_hidden1_weights.append(Weight(float((total_weights[w])[0])))
-            input_to_hidden2_weights.append(Weight(float((total_weights[w])[1])))
-
-        if debug:
-            print("input to hidden neuron 1 weights:")
-            for w in range(0, 4):
-                print(input_to_hidden1_weights[w].val)
-            print()
-            print("input to hidden neuron 2 weights:")
-            for w in range(0, 4):
-                print(input_to_hidden2_weights[w].val)
-
-
-
-    def print_input_layer(self):
-        for input_layer in self.input_layers:
-            for input_neuron in input_layer:
-                print(input_neuron.val, end="", flush=True)
-            print()
-
-
-ann = ANN()
-ann.generate_test_set()
-
-ann.print_input_layer()
-
-
+forward_propagation()
+backward_propagation()
